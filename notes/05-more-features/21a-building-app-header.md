90c03b0b3f1a58a19c7815dd9df33a456b34a8ce
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-02-21 13:42:58 +0700

    feat: upload image in supabase

--- notes/05-more-features/21a-building-app-header.md
+++ notes/05-more-features/21a-building-app-header.md
@@ -1,3 +1,3 @@
 # Building app Header
 
-  
+Next: [Updating user data and password](./22-updating-user-data-and-password.md)
---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateCurrentUser } from "../../services/apiAuthServices";
+
+export function useUpdateUser() {
+  const qureyClient = useQueryClient();
+
+  const { mutate: updateUser, isPending: updating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: ({ user }) => {
+      toast.success("User Account Succefully updating");
+      qureyClient.setQueryData("user", user);
+
+      qureyClient.invalidateQueries({ queryKey: ["user"] });
+    },
+    onError: (error) => toast.error(error.message),
+  });
+
+  return { updateUser, updating };
+}
