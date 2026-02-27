1a4788626684b87b09e7308089d9ca694299b556
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-02-27 14:25:57 +0700

    feat: delete credential

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("bani@example.com");
-  const [password, setPassword] = useState("141102");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, loginLoading } = useLogin();
 
   function handleSubmit(event) {
